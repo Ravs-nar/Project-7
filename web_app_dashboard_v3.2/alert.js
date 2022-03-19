7d0628cace62c6ff161710a538f1ca056294a8d2
Ravi Narayan <ravi.narayan09@gmail.com>
2022-03-19 10:49:00 -0700

    8

--- web_app_dashboard_v3.2/alert.js
+++ web_app_dashboard_v3.2/alert.js
@@ -6,7 +6,7 @@ alertBanner.innerHTML =
 `<div class="alert-banner">
 <p><strong>Alert :</strong> You have <strong>6</strong> overdue tasks
 to complete</p>
-<p class="alert-banner-close">x</p>
+<p  style = "padding-left: 20px;"class="alert-banner-close">x</p>
 </div>
 ` 
   
