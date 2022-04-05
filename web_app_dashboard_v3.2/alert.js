0a7097032c0758be9ebb1d25043213a16280e018
Ravi Narayan <ravi.narayan09@gmail.com>
2022-04-05 11:53:38 -0700

    1

--- web_app_dashboard_v3.2/alert.js
+++ web_app_dashboard_v3.2/alert.js
@@ -4,8 +4,7 @@ const alertBanner = document.getElementById("alert");
 ///create the html for the banner
 alertBanner.innerHTML =
 `<div class="alert-banner">
-<p><strong>Alert :</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
+<p><strong>Alert :</strong> You have unread messages</p>
 <p  style = "padding-left: 20px;"class="alert-banner-close">x</p>
 </div>
 ` 
